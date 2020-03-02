489844fdf24d8cd287d718d88788bf162dde367c
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-03-02 19:42:03 +0530

    general setup for c

---
+++ data/boilerplates/general.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	return (EXIT_SUCCESS);
+}
