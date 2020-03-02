fd2f28243b129b81cf2f86a3a917c1d440ccee5d
Mihir Luthra <1999mihir.luthra@gmail.com>
2020-03-03 00:38:11 +0530

    use sh specifics in source file

--- to_source.sh
+++ to_source.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
 if [ -n "${__source_guard__tosourcebash__}" ] ; then
 	return 0
@@ -7,8 +7,15 @@ else
 fi
 
 
+if [ -n "${BASH_VERSION}" ] ; then
+	SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE}" )" ; pwd )"
+elif [ -n "${ZSH_VERSION}" ] ; then
+	SCRIPT_PATH=$( dirname "$0:A" )
+fi
+
+
 if [ -z "${QUICKTEST_PATH}" ] ; then
-	export QUICKTEST_PATH="$( cd "$( dirname "${BASH_SOURCE}" )" ; pwd )"
+	export QUICKTEST_PATH="${SCRIPT_PATH}"
 	export PATH="${QUICKTEST_PATH}/commands:${PATH}"
 fi
 
